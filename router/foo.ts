b58c2948a3c01bbfe4802b75c73a2cb07bba0d79
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-03-06 10:02:02 +0000

    introduce neo app skeleton

--- router/foo.ts
+++
@@ -1,8 +0,0 @@
-import fastify from 'fastify';
-
-const app = fastify({ logger: { name: 'foo', level: 'debug' } });
-
-app.get('/', () => "I'm foo.");
-app.get('/ping', () => 'pong');
-
-app.listen({ host: '0.0.0.0', port: 3001 });
--- router/main.ts
+++ router/main.ts
@@ -1,79 +1,44 @@
 import fastify from 'fastify';
 import { LRUCache } from 'lru-cache';
-import type { IncomingMessage } from 'node-types/http';
 import { pino } from 'pino';
-import { type ProxyOptions, Redbird } from 'redbird';
 
-const GATE_PORT = Number(Deno.env.get('GATE_PORT') ?? '3000');
-if (!Number.isInteger(GATE_PORT)) {
-  throw new Error('!Number.isInteger(GATE_PORT)');
-}
-const META_PORT = Number(Deno.env.get('META_PORT') ?? '3001');
-if (!Number.isInteger(META_PORT)) {
-  throw new Error('!Number.isInteger(META_PORT)');
+const PORT = Number(Deno.env.get('PORT') ?? '3001');
+if (!Number.isInteger(PORT)) {
+  throw new Error('!Number.isInteger(PORT)');
 }
 
 const logger = pino({ name: 'main', level: 'debug' });
 
-const routes = new LRUCache<string, string>({ max: 1_000_000 });
+const cache = new LRUCache<string, string>({ max: 1_000_000 });
 
-const resolve: NonNullable<ProxyOptions['resolvers']>[0]['fn'] = (
-  _host: string,
-  url: string,
-  req?: IncomingMessage,
-) => {
-  if (!req) return null;
+const app = fastify({ logger: { name: 'fastify' } });
 
-  if (url === '/meta' || url.startsWith('/meta/')) {
-    return `http://127.0.0.1:${META_PORT}`;
-  }
+app.get('/', () => 'pong');
+app.get('/health', () => 'healthy');
 
-  const query = new URL(url, 'http://dummy').searchParams;
-  logger.debug(Array.from(query));
-  const key = query.get('key');
-  if (key && routes.has(key)) {
-    return routes.get(key) as string;
-  }
-
-  req.method = 'GET';
-  req.url = '/fallback/unknown';
-  return `http://127.0.0.1:${META_PORT}`;
-};
-
-new Redbird({
-  port: GATE_PORT,
-  host: '0.0.0.0',
-  logger: pino({ name: 'proxy', level: 'debug' }),
-  resolvers: [{ fn: resolve, priority: 50 }],
-});
-
-const metaApp = fastify({ logger: { name: 'metaApp', level: 'debug' } });
-
-metaApp.get('/meta', () => 'healthy');
-
-metaApp.get('/fallback/unknown', (_, reply) => {
+app.get('/404', (_, reply) => {
   reply.code(404).send('unknown');
 });
 
-// $ curl -i -X POST http://127.0.0.1:6780/meta/test/good\?foo\=bar\&foo\=baz -H 'Content-Type: application/json' -d '{"arr":[1,2,3]}'
-metaApp.post<{
+// $ curl -i -X POST http://127.0.0.1:6780/test/good\?foo\=bar\&foo\=baz -H 'Content-Type: application/json' -d '{"arr":[1,2,3]}'
+app.post<{
   Params: { id: string };
   Querystring: { foo?: string | string[] };
   Body: { arr: number[] };
   Reply: 'done';
-}>('/meta/test/:id', (req, _reply) => {
+}>('/test/:id', (req, _reply) => {
   logger.debug({ params: req.params, body: req.body, query: req.query });
   return 'done';
 });
 
-metaApp.get<{
+app.get<{
   Reply: [string, string][];
-}>('/meta/routes', () => Array.from(routes.entries()));
+}>('/cache', () => Array.from(cache.entries()));
 
-metaApp.post<{
+app.post<{
   Body: { key?: string };
   Reply: 'ok';
-}>('/meta/routes', (req, reply) => {
+}>('/cache', (req, reply) => {
   if (!req.body.key || typeof req.body.key !== 'string') {
     reply.code(400).send();
     return;
@@ -81,20 +46,20 @@ metaApp.post<{
 
   logger.debug(req.body);
 
-  routes.set(req.body.key, 'http://127.0.0.1:3001');
+  cache.set(req.body.key, 'up');
 
   return 'ok';
 });
 
-metaApp.delete<{
+app.delete<{
   Params: { key: string };
   Reply: string | null;
-}>('/meta/routes/:key', (req) => {
-  const value = routes.get(req.params.key);
-  routes.delete(req.params.key);
+}>('/cache/:key', (req) => {
+  const value = cache.get(req.params.key);
+  cache.delete(req.params.key);
   return value || null;
 });
 
-metaApp
-  .listen({ port: META_PORT, host: '0.0.0.0' })
+app
+  .listen({ port: PORT, host: '0.0.0.0' })
   .catch(logger.error);
